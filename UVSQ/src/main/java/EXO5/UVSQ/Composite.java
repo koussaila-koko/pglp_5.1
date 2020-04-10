f8617eb57434b8b4cec28c33f21ec74759d9f05a
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-04-10 17:27:17 +0200

    Create Composite.java

---
+++ UVSQ/src/main/java/EXO5/UVSQ/Composite.java
@@ -0,0 +1,12 @@
+package EXO5.UVSQ;
+/**
+ * Interface Composite.
+ * @author koussaila
+ */
+public interface Composite {
+	  /**
+	   * la methode print
+     * La renvoie sous forme de chaine de caractère.
+     */
+	public void print();
+	}
