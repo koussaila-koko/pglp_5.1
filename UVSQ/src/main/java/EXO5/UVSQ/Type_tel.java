253d100e63dd405cf0b85be79de131e84f8ff2e4
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-04-10 17:27:56 +0200

    Create Type_tel.java

---
+++ UVSQ/src/main/java/EXO5/UVSQ/Type_tel.java
@@ -0,0 +1,20 @@
+package EXO5.UVSQ;
+/**
+ * enum Type_tel .
+ * les 3 type de telphone possible
+ * @author koussaila
+ */
+public enum Type_tel {
+	/**
+	 * le numero fix_pro .
+	 */
+   fix_pro,
+   /**
+	 * le numero fix_perso .
+	 */
+   fix_perso,
+   /**
+	 * le numero portable .
+	 */
+   portable;
+}
